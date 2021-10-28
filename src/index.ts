fa24ea5d87078159d97dd3862c0a95341e046e7b
Ettore <ettore.barbosa@gmail.com>
2021-10-28 13:34:50 -0300

    swagger file

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import swagger from "swagger-ui-express";
 
+import Swagger from "../Swagger.json/";
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
 
 app.use(express.json());
 
+app.use("/api-docs", swagger.serve, swagger.setup(Swagger));
 app.use("/users", usersRoutes);
 
 export { app };
